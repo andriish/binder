944470a187e8467d7fb69e9bcf12f97eb75d977c
Andrii Verbytskyi <averbyts@cern.ch>
2020-09-04 13:31:08 +0200

    Try4

--- .github/workflows/MSYS2/entrypoint.sh
+++ .github/workflows/MSYS2/entrypoint.sh
@@ -1,3 +1,8 @@
+
+ls D:/a/_temp/msys/msys64/mingw64/bin/
+ls D:/a/_temp/msys/msys64/mingw64/lib/
+ls D:/a/_temp/msys/msys64/mingw64/lib64/
+
 cmake CMakeLists.txt -DBINDER_TEST_PYTHON_VERSIONS=0
 make
 ldd source/binder
