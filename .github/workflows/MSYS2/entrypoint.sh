7e0e02b9d019f583cc12fa91e58492659b5c078c
Andrii Verbytskyi <averbyts@cern.ch>
2020-09-04 14:47:52 +0200

    Try6

--- .github/workflows/MSYS2/entrypoint.sh
+++ .github/workflows/MSYS2/entrypoint.sh
@@ -8,7 +8,7 @@ ls D:/a/_temp/msys/msys64/mingw64/bin/libLLVM.*
 ls D:/a/_temp/msys/msys64/mingw64/lib/libLLVM.*
 
 
-cmake CMakeLists.txt -DBINDER_TEST_PYTHON_VERSIONS=0
+cmake CMakeLists.txt -DBINDER_TEST_PYTHON_VERSIONS=0  -DCMAKE_CXX_COMPILER=g++  -DCMAKE_C_COMPILER=gcc -G "MinGW Makefiles"
 cmake --build . 
 ldd source/binder
 ctest 
