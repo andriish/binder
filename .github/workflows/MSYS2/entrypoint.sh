0d5490c2594323fb28ef72fce2b54dfb24d41714
Andrii Verbytskyi <averbyts@cern.ch>
2020-09-04 13:48:57 +0200

    Try4

--- .github/workflows/MSYS2/entrypoint.sh
+++ .github/workflows/MSYS2/entrypoint.sh
@@ -1,7 +1,12 @@
+set -x
+#ls D:/a/_temp/msys/msys64/mingw64/bin/
+#ls D:/a/_temp/msys/msys64/mingw64/lib/
+#ls D:/a/_temp/msys/msys64/mingw64/lib64/
 
-ls D:/a/_temp/msys/msys64/mingw64/bin/
-ls D:/a/_temp/msys/msys64/mingw64/lib/
-ls D:/a/_temp/msys/msys64/mingw64/lib64/
+ls D:/a/_temp/msys/msys64/mingw64/bin/libLLVM.*
+
+ls D:/a/_temp/msys/msys64/mingw64/lib/libLLVM.*
+
 
 cmake CMakeLists.txt -DBINDER_TEST_PYTHON_VERSIONS=0
 make
