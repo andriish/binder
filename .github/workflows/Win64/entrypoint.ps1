40b7b809e84c4479be864112881fcbd440b68f97
Andrii Verbytskyi <averbyts@cern.ch>
2020-09-04 15:36:46 +0200

    OK

--- .github/workflows/Win64/entrypoint.ps1
+++ .github/workflows/Win64/entrypoint.ps1
@@ -1,5 +1,10 @@
 echo "HALLO"
-cmake CMakeLists.txt -DBINDER_TEST_PYTHON_VERSIONS=0 
+
+wget 'https://ziglang.org/deps/llvm+clang+lld-10.0.0-x86_64-windows-msvc-release-mt.tar.xz'
+
+Expand-Archive -LiteralPath 'llvm+clang+lld-10.0.0-x86_64-windows-msvc-release-mt.tar.xz'
+
+cmake CMakeLists.txt -DBINDER_TEST_PYTHON_VERSIONS=0  -DCMAKE_PREFIX_PATH=llvm+clang+lld-10.0.0-x86_64-windows-msvc-release-mt
 cmake --build . 
 ldd source/binder
 ctest 
