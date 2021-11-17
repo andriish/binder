22f4e7d9be62323f7746319ee2f9f86f64a166aa
Andrii Verbytskyi <averbyts@cern.ch>
2021-11-17 10:19:01 +0100

    Ubuntu

--- .github/workflows/ubuntu18/entrypoint.sh
+++ .github/workflows/ubuntu18/entrypoint.sh
@@ -10,7 +10,7 @@ apt-get -y install  cmake make gcc
 apt-get -y install  pybind11-dev python python-dev 
 ls /usr/lib/llvm-6.0/bin/../lib/
 
-cmake CMakeLists.txt -DCMAKE_FIND_STATIC=LAST
+cmake CMakeLists.txt -DCMAKE_FIND_STATIC=LAST -DCMAKE_FIND_LIBRARY_SUFFIXES=.so
 make
 ldd source/binder
 ldd -u -r source/binder
