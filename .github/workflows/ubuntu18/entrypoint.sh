cf94729211780a7a775105386565cceb380bd10b
Andrii Verbytskyi <averbyts@cern.ch>
2021-11-17 08:56:41 +0100

    Ubuntu

---
+++ .github/workflows/ubuntu18/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -l
+set -x
+uname -a 
+cat /etc/issue
+apt-get -y git  zlib1g zlib1g-dev ncurses-dev  clang libclang-dev clang-libs  llvm llvm-dev
+apt-get -y install  cmake make gcc gcc-c++
+apt-get -y install  pybind11-dev python python-dev 
+
+cmake CMakeLists.txt
+make
+ldd source/binder
+ldd -u -r source/binder
+ctest . --output-on-failure 
+out=$?
+cat Testing/Temporary/LastTest.log
+echo ::set-output name=out::$out
