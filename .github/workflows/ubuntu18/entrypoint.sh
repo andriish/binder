e603108c166bf81bd294ce1b103f9dc531619f39
Andrii Verbytskyi <averbyts@cern.ch>
2021-11-17 09:58:46 +0100

    Ubuntu

--- .github/workflows/ubuntu18/entrypoint.sh
+++ .github/workflows/ubuntu18/entrypoint.sh
@@ -5,9 +5,10 @@ cat /etc/issue
 apt -y update
 add-apt-repository -y universe multiverse
 apt -y update
-apt-get -y install  git  zlib1g zlib1g-dev ncurses-dev  clang libclang-dev   llvm llvm-dev
+apt-get -y install  git  zlib1g zlib1g-dev ncurses-dev clang llvm libclang-dev llvm-dev llvm-runtime
 apt-get -y install  cmake make gcc 
 apt-get -y install  pybind11-dev python python-dev 
+ls /usr/lib/llvm-6.0/bin/../lib/
 
 cmake CMakeLists.txt
 make
