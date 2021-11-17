990ba7c6c872db3c9ea40188e56075ef7090f0e0
Andrii Verbytskyi <averbyts@cern.ch>
2021-11-17 09:48:46 +0100

    Ubuntu

--- .github/workflows/ubuntu18/entrypoint.sh
+++ .github/workflows/ubuntu18/entrypoint.sh
@@ -2,6 +2,9 @@
 set -x
 uname -a 
 cat /etc/issue
+apt -y update
+add-apt-repository -y universe multiverse
+apt -y update
 apt-get -y install  git  zlib1g zlib1g-dev ncurses-dev  clang libclang-dev clang-libs  llvm llvm-dev
 apt-get -y install  cmake make gcc gcc-c++
 apt-get -y install  pybind11-dev python python-dev 
